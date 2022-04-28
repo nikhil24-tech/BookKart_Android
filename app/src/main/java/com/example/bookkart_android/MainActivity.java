420228ddb823d4ff61f8e00b19f2a352daf580f4
navdeep-stack <navdeepbajwa0034@gmail.com>
2022-04-28 19:07:50 -0400

    SignIn/Signup Screens commit V1.

--- app/src/main/java/com/example/bookkart_android/MainActivity.java
+++ app/src/main/java/com/example/bookkart_android/MainActivity.java
@@ -1,14 +1,35 @@
 package com.example.bookkart_android;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+
+import com.example.bookkart.databinding.ActivityMainBinding;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+//        CustomFragmentStateAdapter adapter = new CustomFragmentStateAdapter(this);
+//        binding.viewPager.setAdapter(adapter);
+        binding.viewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                if (position == 3) {
+                    binding.title.setTitle("My Profile");
+                } else {
+                    binding.title.setTitle("Book Kart");
+                }
+            }
+        });
+        new TabLayoutMediator(binding.tabs, binding.viewPager, (tab, position) -> {
+            tab.setText(null);
+//            tab.setIcon(adapter.getDrawable(position));
+        }).attach();    }
 }
---
+++ app/src/main/java/com/example/bookkart_android/SignUpOptionsActivity.java
@@ -0,0 +1,36 @@
+package com.example.bookkart_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.example.bookkart.databinding.ActivitySignUpOptionsBinding;
+import com.example.bookkart_android.MainActivity;
+import com.example.bookkart_android.SignInActivity;
+
+public class SignUpOptionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivitySignUpOptionsBinding binding = ActivitySignUpOptionsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.emailSignIn.setOnClickListener(v -> {
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
+        });
+        binding.googleSignIn.setOnClickListener(v -> {
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
+        });
+        binding.facebookSignIn.setOnClickListener(v -> {
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
+        });
+        binding.goToSignIn.setOnClickListener(v -> {
+            startActivity(new Intent(this, SignInActivity.class));
+            finish();
+        });
+    }
+}
